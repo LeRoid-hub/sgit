918a0f83e12cf40ce3fd94a186ae9db5ff70c9ab
Jan Barfuß <janbar2001@gmail.com>
2024-03-10 13:11:53 +0100

    some more but file is folder

--- sgit.go
+++ sgit.go
@@ -1,35 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"log"
 	"flag"
+	"fmt"
+	"log"
+	"os"
 	"path/filepath"
-	)
+)
 
 type Repository struct {
 	workdir string
-	gitdir string
+	gitdir  string
 	confdir string
-
+	conf    string
 }
 
-var rep Repository
-var forced *bool
+var (
+	rep    Repository
+	forced *bool
+)
 
 func main() {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
-	    log.Fatal(err)
+		log.Fatal(err)
 	}
-	fmt.Println(dir)
+	fmt.Println("mydir" + dir)
 	forced = flag.Bool("forced", false, "Use Forced mode")
-
 
 	flag.Parse()
 	fmt.Println("forced:", *forced)
 	fmt.Println("tail:", flag.Args())
+	fmt.Println("------------------")
 
 	args := flag.Args()
 
@@ -38,35 +40,100 @@ func main() {
 		os.Exit(1)
 	}
 
-
 	fmt.Println(args)
 
 	switch args[0] {
-		case "init": cmd_init()
-		default: fmt.Println("Invalid command")
+	case "init":
+		cmd_init(&dir)
+	default:
+		fmt.Println("Invalid command")
 	}
 }
 
-func cmd_init() {
+func cmd_init(path *string) {
 	*forced = true
 
+	fmt.Println("path:", *path)
 	fmt.Println("Initializing sgit")
+	repo_init(path, *forced)
+}
+
+func repo_init(path *string, force bool) {
+	rep.workdir = *path
+	rep.gitdir = filepath.Join(rep.workdir, ".sgit")
+
+	file, err := os.Stat(rep.workdir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check if Git repository exists
+	if !force || !file.IsDir() {
+		mess := "Not a Git repository: " + rep.workdir
+		panic(mess)
+	}
+
+	// Read configuration file
+	rep.confdir = repo_file("config/")
+	fmt.Println(rep)
+
+	fmt.Println("repo_init")
+}
+
+func repo_path(path string) string {
+	return filepath.Join(rep.gitdir, path)
+}
+
+func opt_bool(opt []bool) bool {
+	if len(opt) > 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func repo_file(path string, opt ...bool) string {
+	mkdir := opt_bool(opt)
+
+	fmt.Println("path:", path)
+	p := (path)[:len(path)-1]
+
+	if repo_dir(p, mkdir) != "" {
+		return repo_path(path)
+	}
+	return ""
+}
+
+func repo_dir(path string, opt ...bool) string {
+	path = repo_path(path)
+	mkdir := opt_bool(opt)
+
+	file, err := os.Stat(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(file)
+
+	if file != nil {
+		if file.IsDir() {
+			return path
+		} else {
+			message := "Not a directory: " + path
+			panic(message)
+		}
+	}
+
+	if mkdir {
+		err := os.MkdirAll(path, 0777)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Create directory")
+		return path
+	}
+	return ""
 }
 
 func repo_create() {
 	fmt.Println("Creating repository")
 }
-
-func repo_path(repo Repository, path *string) {
-	fmt.Println("repo_path")
-}
-
-func repo_file(repo Repository, path *string) {
-	fmt.Println("repo_file")
-
-}
-
-func repo_dir(repo Repository, path *string, mkdir bool) {
-	fmt.Println("repo_dir")
-}
-
